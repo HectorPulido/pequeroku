d6622e09eb3eb91eec05ae5926075fcb82c3b79c
Hector Pulido <hectorandrespp@gmail.com>
2025-06-25 10:46:09 -0500

    admin account

--- source/entrypoint.sh
+++ source/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py collectstatic --no-input
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+$@
+
 gunicorn pequeroku.wsgi:application --bind 0.0.0.0:8000  --timeout 240 --workers 3 --log-level=debug
