41900c91bedbe59b7d4d0b31a4c6c6e9059f342a
Hector Pulido <hectorandrespp@gmail.com>
2025-09-15 19:12:40 -0500

    small fix front

--- source/front/js/core/utils.js
+++ source/front/js/core/utils.js
@@ -24,7 +24,7 @@ export function hideHeader() {
 	header.classList.add("hidden");
 
 	try {
-		document.querySelector(".main").classList.remove("calc");
+		document.querySelector("main").classList.remove("calc");
 	} catch {}
 }
 
